63a9e48f78fc662bd350310e6f13b96592fe64af
Ghiță Bizău <ghita_bizau@yahoo.com>
2019-03-17 14:57:58 +0200

    Add entry point.

--- src/rvs_scraper/scraper.py
+++ src/rvs_scraper/scraper.py
@@ -1,6 +1,6 @@
 # import libraries
-# import pdb; pdb.set_trace()
-# import urllib2
-# from bs4 import BeautifulSoup
+import pdb; pdb.set_trace()
+import urllib2
+from bs4 import BeautifulSoup
 
 print "HELLO WORLD!!"
