4f286609d2e78918ef33172311c079a61942a8f1
Ghiță Bizău <ghita_bizau@yahoo.com>
2019-03-17 16:14:24 +0200

    Get songs data.

--- src/rvs_scraper/scraper.py
+++ src/rvs_scraper/scraper.py
@@ -2,10 +2,19 @@ import urllib2
 from bs4 import BeautifulSoup
 
 
+data = {}
+
+
 def main():
     url = "http://www.rvs.ro/"
     page = urllib2.urlopen(url)
     soup = BeautifulSoup(page, "html.parser")
 
     music_div = soup.find("div", {"id": "melodii"})
-    import pdb; pdb.set_trace()
+    songs = music_div.find("ul", {"class": "songs"}).findAll("li")
+
+    for song in songs:
+        song_title = song.text.strip()
+        data[song_title] = song_title
+
+    print data
