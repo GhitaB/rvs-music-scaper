f54a1a1c7da6215d42eef6561fd197455aa91f1b
Ghiță Bizău <ghita_bizau@yahoo.com>
2019-03-17 16:36:18 +0200

    Get mp3s url.

--- src/rvs_scraper/scraper.py
+++ src/rvs_scraper/scraper.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 data = {}
 
 
-def main():
+def scrap_once():
     url = "http://www.rvs.ro/"
     page = urllib2.urlopen(url)
     soup = BeautifulSoup(page, "html.parser")
@@ -13,8 +13,21 @@ def main():
     music_div = soup.find("div", {"id": "melodii"})
     songs = music_div.find("ul", {"class": "songs"}).findAll("li")
 
+    new = 0
     for song in songs:
         song_title = song.text.strip()
-        data[song_title] = song_title
+        if data.get(song_title, None) is None:
+            new += 1
+        song_url = song.findAll()[0].attrs['data-url']
+        data[song_title] = song_url
+
+    return new
+
+
+def main():
+    new = 1
+    while new != 0:
+        new = scrap_once()
+        print new
 
     print data
